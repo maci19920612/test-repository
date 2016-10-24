054db9ee8d27b4b05ecdf11974a2cad0e46e87bb
Földesi Dávid <f.david314@gmail.com>
2016-10-24 20:35:08 +0200

    asdkjasdasdóasdkjasdkjasdkj

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class Main{
 	public static void main(String... args){
 		System.out.println("Hello World!");
 		System.out.println("Sorital");
+		List<String> names = new ArrayList<>();
+		names.add("Help1");
 	}
 }
